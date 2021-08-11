dd3911933f382744b1ca5120324c393a02f8bed7
alann-maulana <alann_it06@yahoo.co.id>
2021-08-12 00:06:22 +0700

    add example

---
+++ flutter_parse_storage_interface/example/flutter_parse_storage_interface_example.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_parse_storage_interface/flutter_parse_storage_interface.dart';
+
+class InMemoryFlutterParseStorage extends FlutterParseStorageInterface {
+  /// Instantiates an empty in-memory preferences store.
+  InMemoryFlutterParseStorage.empty()
+      : _data = <String, Map<String, Object?>>{};
+
+  /// Instantiates an in-memory preferences store containing a copy of [data].
+  InMemoryFlutterParseStorage.withData(Map<String, Map<String, Object?>> data)
+      : _data = Map<String, Map<String, Object>>.from(data);
+
+  final Map<String, Map<String, Object?>> _data;
+
+  @override
+  Future<bool> clear({String? key}) async {
+    if (key != null) {
+      _data.remove(key);
+    } else {
+      _data.clear();
+    }
+    return true;
+  }
+
+  @override
+  Future<Map<String, Object?>?> get(String key) async {
+    return _data[key];
+  }
+
+  @override
+  Future<bool> put(String key, Map<String, Object?> data) async {
+    _data[key] = data;
+    return true;
+  }
+}
